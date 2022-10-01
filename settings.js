f7b191cebad2326528fa7f5b680c3e23a32e2375
wr3dyard <114644483+wr3dyard@users.noreply.github.com>
2022-10-01 12:16:42 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,7 +36,7 @@ global.socialm = "GitHub: prabhasha2006" // github or insta name
 global.location = "Sri Lanka,Ampara" // location
 
 //bot body 
-global.alivepic = `https://i.ibb.co/cyyCjN7/20220803-081426.png` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
+global.alivepic = `https://i.ibb.co/VqMWkBv/IMG-20221001-WA0001.jpg` // alive picture \\ use "https://ibb.com" to upload photos. < 1 MB
 
 global.dragonchat = "false"  // chat bot  "true" / "false" (200 auto replies in this)
 global.callblock = "false" // call block "true" / "false" ("true"  to block callers)
@@ -50,9 +50,9 @@ global.welcom_msg = "true" // Welcome / Goodbye   [true/false]
 global.owner = ['+380944814219']
 global.ownertag = '+380944814219' //your tag number
 global.botname = '𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉 - 𝘉𝘖𝘛' //ur bot name
-global.packname = "Bot Sticker" // sticker packname
+global.packname = "𝚂𝚃𝙸𝙲𝙺𝙴𝚁 𝚋𝚢 𝚂𝙷𝙴𝙷𝙰𝙽" // sticker packname
 global.author = "𝙍𝙀𝘿╺ 𝘿𝙍𝘼𝙂𝙊𝙉" // sticker author
-global.dragonemoji = '🧩' // Menu emoji
+global.dragonemoji = '🐉' // Menu emoji
 global.footer = '[ ◉ ʀᴇᴅ-ᴅʀᴀɢᴏɴ-ᴍᴅ ]' //
 
 //database
